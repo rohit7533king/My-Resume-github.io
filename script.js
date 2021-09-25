672400a12a847cc71579b073e09fb498a37b8ea6
Rohit Pakhare <82589278+rohit7533king@users.noreply.github.com>
2021-09-25 22:03:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function welcome () {
+window.open ("https://github.com/rohit7533king/Speech-detection-js");
+}
+
+function myfunc () {
+ window.open ("https://github.com/rohit7533king/Mouse-shadow-with-js");
+}
+
+function newfunc () {
+window.open ("https://github.com/rohit7533king/Html-video-player");
+}
+
+function oldfunc () {
+ window.open ("https://github.com/rohit7533king/Key-Sequence-Detection");
+}
